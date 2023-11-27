967cfc75596ad27e0fe2eaf7ce2cf7dd53058e60
William Ndubuisi <princewill835@gmail.com>
2023-11-27 17:40:54 +0100

    plans checkout created

--- public/source/js/pages/plan.js
+++ public/source/js/pages/plan.js
@@ -2,7 +2,56 @@ const productPrice = document.querySelector('#initial-price');
 const addOn = document.querySelector('#addon');
 const oneTime = document.querySelector('#onetime');
 const admin = document.querySelector('#admin');
-const subTotal= document.querySelector('#subTotal');
-const subTotalHTML = subTotal.innerHTML
-const calculatedSubTotal =95
-// console.log(subTotalHTML)
+const subTotal = document.querySelector('#subTotal');
+const dropdown = document.getElementById('inputGroupSelect04');
+const discountTextElement = document.getElementById('discount-text');
+const discountedPriceElement = document.getElementById('discounted-price');
+
+const subTotalHTML = subTotal.innerHTML;
+const extractedProductPrice = productPrice.getAttribute('data-initial-price');
+const extractedAddOn = addOn.getAttribute('data-addon');
+const extractedOneTime = oneTime.getAttribute('data-addon'); 
+const extractedAdmin = admin.getAttribute('data-admin');
+
+function updatePrice(newProductPrice) {
+	const calculatedTotal =
+		parseInt(newProductPrice) +
+		parseInt(extractedAddOn) +
+		parseInt(extractedOneTime) +
+		parseInt(extractedAdmin);
+	subTotal.innerHTML = calculatedTotal;
+}
+
+function hideDiscountText() {
+	discountTextElement.style.display = 'none';
+	discountedPriceElement.innerText = productPrice.innerHTML;
+	updatePrice(extractedProductPrice);
+}
+
+function showDiscountText(discount) {
+	const discountedPrice = (49 - 49 * (discount / 100)).toFixed(0);
+	const discountText = discount + '% off';
+
+	discountedPriceElement.innerText = '$' + discountedPrice;
+	discountTextElement.innerText = discountText;
+	discountTextElement.style.display = 'block';
+	updatePrice(discountedPrice);
+}
+
+function updateDiscount() {
+	const selectedOption = dropdown.options[dropdown.selectedIndex];
+
+	if (selectedOption.value === '0') {
+		hideDiscountText();
+	} else {
+		const discount = selectedOption.getAttribute('data-discount');
+		showDiscountText(discount);
+	}
+}
+
+// Initial setup
+updatePrice(extractedProductPrice);
+discountTextElement.style.display = 'none';
+
+// Event listener for dropdown change
+dropdown.addEventListener('change', updateDiscount);
