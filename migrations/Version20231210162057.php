68f99f2223ffc13faaa8a1e12d2c65e875f5464b
William Ndubuisi <princewill835@gmail.com>
2023-12-10 19:16:56 +0100

    git userprofile created

---
+++ migrations/Version20231210162057.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231210162057 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD bio VARCHAR(255) NOT NULL, ADD company_name VARCHAR(255) NOT NULL, ADD company_website VARCHAR(255) NOT NULL, ADD facebook VARCHAR(255) NOT NULL, ADD twitter VARCHAR(255) NOT NULL, ADD linkedin VARCHAR(255) NOT NULL, ADD skype VARCHAR(255) NOT NULL, ADD github VARCHAR(255) NOT NULL, ADD profile_picture VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP bio, DROP company_name, DROP company_website, DROP facebook, DROP twitter, DROP linkedin, DROP skype, DROP github, DROP profile_picture');
+    }
+}
---
+++ src/Entity/Account/UserProfile.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace App\Entity\Account;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\HRMSystem\UserProfileRepository;
+
+#[ORM\Entity(repositoryClass: UserProfileRepository::class)]
+class UserProfile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(targetEntity: "User")]
+    private ?User $user = null;
+
+    #[ORM\Column(type: "text")]
+    private ?string $bio = null;    
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $companyName = null;
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $companyWebsite = null;
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $facebook = null;
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $twitter = null;
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $instagram = null;
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $linkedin = null;
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $skype = null;
+
+    #[ORM\Column(length: 180, type: "text")]
+    private ?string $github = null;
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getBio(): ?string
+    {
+        return $this->bio;
+    }
+
+    public function setBio(string $bio): static
+    {
+        $this->bio = $bio;
+
+        return $this;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(string $companyName): static
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
+    public function getCompanyWebsite(): ?string
+    {
+        return $this->companyWebsite;
+    }
+
+    public function setCompanyWebsite(string $companyWebsite): static
+    {
+        $this->companyWebsite = $companyWebsite;
+
+        return $this;
+    }
+
+    public function getFacebook(): ?string
+    {
+        return $this->facebook;
+    }
+
+    public function setFacebook(string $facebook): static
+    {
+        $this->facebook = $facebook;
+
+        return $this;
+    }
+
+    public function getTwitter(): ?string
+    {
+        return $this->twitter;
+    }
+
+    public function setTwitter(string $twitter): static
+    {
+        $this->twitter = $twitter;
+
+        return $this;
+    }
+
+    public function getInstagram(): ?string
+    {
+        return $this->instagram;
+    }
+
+    public function setInstagram(string $instagram): static
+    {
+        $this->instagram = $instagram;
+
+        return $this;
+    }
+
+    public function getLinkedin(): ?string
+    {
+        return $this->linkedin;
+    }
+
+    public function setLinkedin(string $linkedin): static
+    {
+        $this->linkedin = $linkedin;
+
+        return $this;
+    }
+
+    public function getSkype(): ?string
+    {
+        return $this->skype;
+    }
+
+    public function setSkype(string $skype): static
+    {
+        $this->skype = $skype;
+
+        return $this;
+    }
+
+    public function getGithub(): ?string
+    {
+        return $this->github;
+    }
+
+    public function setGithub(string $github): static
+    {
+        $this->github = $github;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
