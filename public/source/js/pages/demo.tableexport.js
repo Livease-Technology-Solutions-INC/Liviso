547f43e78886dd9267dc6a9c877e308deca10bd1
Ajita9496 <asthanaajita9496@gmail.com>
2024-02-12 20:02:10 +0530

    table-export functionality added

--- public/source/js/pages/demo.tableexport.js
+++ public/source/js/pages/demo.tableexport.js
@@ -1,17 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const exportBtns = document.querySelectorAll(".export-btn");
 
-    document.addEventListener("DOMContentLoaded", function() {
-        const exportBtns = document.querySelectorAll(".export-btn");
-
-        exportBtns.forEach(btn => {
-            btn.addEventListener("click", function() {
-                const tableId = this.getAttribute("data-table-id");
-                const table = document.getElementById(tableId);
-                exportTableToExcel(table, `table_${tableId}.xlsx`);
-            });
+    exportBtns.forEach(btn => {
+        btn.addEventListener("click", function() {
+            const tableId = this.getAttribute("data-table-id");
+            const tableName = this.getAttribute("data-table-name");
+            const table = document.getElementById(tableId);
+            exportTableToExcel(table, `${tableName}.xlsx`);
         });
-
-        function exportTableToExcel(table, filename) {
-            const wb = XLSX.utils.table_to_book(table, {sheet: "Sheet JS"});
-            XLSX.writeFile(wb, filename);
-        }
     });
+
+    function exportTableToExcel(table, filename) {
+        const wb = XLSX.utils.table_to_book(table, {sheet: "Sheet JS"});
+        XLSX.writeFile(wb, filename);
+    }
+});
